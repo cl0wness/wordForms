fb35103403a2098f6f8afbf46a6178aa0d9101db
cl0wness <clown.es@yandex.ru>
2022-05-26 12:02:26 +0300

    разработана функция separByWords

--- functions.h
+++ functions.h
@@ -1,4 +1,14 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include <QString>
+
+/*!
+ *  \brief Разделить строку на слова.
+ *
+ *  Функция делит строку на слова. Разделительные знаки - знаки препинания и пробелы.
+ *  param[in] str - исходная строка
+ *  param[out] words - список слов
+*/
+void separByWords(QString str, QList<QString> &words);
 
 #endif // FUNCTIONS_H
--- main.cpp
+++ main.cpp
@@ -6,3 +6,25 @@ int main(int argc, char *argv[])
 
     return a.exec();
 }
+
+void separByWords(QString str, QList<QString> &words)
+{
+    //..Считать слово не найденным
+    int wordLenght = 0;
+    // Пока поиск не дошёл до конца предложения
+    while (wordLenght != -1)
+    {
+        QString word;
+        //Найти место разделяющего знака (знака препинания или пробела)
+        wordLenght = str.indexOf(QRegExp("[ ,-;.!?\'\"]"));
+        //Скопировать слово до этого знака
+        word = str.left(wordLenght);
+        //Перевести слово в нижний регистр
+        word = word.toLower();
+        //Записать в итоговые слова, если слово имеется
+        if(!word.isEmpty())
+            words << word;
+        //Удалить слово из предложения
+        str.remove(0, wordLenght+1);
+    }
+}
